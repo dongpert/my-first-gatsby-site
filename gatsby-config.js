4498443d6b3d15e511bf1438b45115a0ae2e1b32
Dongwon Seo <dongpert@gmail.com>
2022-02-23 15:08:53 +0900

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+        siteUrl: `https://www.yourdomain.tld`
     },
-    plugins: [
-
-    ]
-}
+    plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp']
+};
