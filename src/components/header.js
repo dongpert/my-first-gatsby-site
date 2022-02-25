2bede4706daaa59af1572a9fe16d66952439270d
Dongwon Seo <dongpert@gmail.com>
2022-02-25 18:02:05 +0900

    Working Gatsby Tutorial Part 4

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+import { useStaticQuery, graphql } from 'gatsby';
+
+const Header = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `);
+
+    return (
+        <header>
+            <h1>{data.site.siteMetadata.title}</h1>
+        </header>
+    );
+};
